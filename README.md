081abdf3880679e09aef464b8085ac12334414c7
GimiTechno <kr84Tsroot@gmail.com>
2021-02-11 14:04:41 +0900

    【SRC/DOC】 ・D-designの新人教育用基板開発環境一式 ➡　回路図、自作サンプルソース ★とりあえず、README.txtを読ませること!!!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# D-design-
+# 【D-design新人教育用基板 REV1.0(非公開:開発用)】
+ 
+★会社には <font color="Red">README.txt</font>を読ませること!!!
+
+## 【機能(ドライバ等) ... REV1.0】
+ 
+・USB　…　VCP（仮想COMポート）でprintf（会社名が出る）
---
+++ STM32_Smart_OJT_PCB/SRC/VER1.0_20201221/STM32_Smart/Core/Inc/Common.h
@@ -0,0 +1,16 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define FALSE 	0x00
+#define TRUE 	0x01
+
+#define LOW 	0x00
+#define HIGH 	0x01
+
+// USR_BUTTON IRQ Flag
+extern uint8_t g_irq_f;
+#endif
